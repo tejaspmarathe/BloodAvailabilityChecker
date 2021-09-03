3875b4fb010621e676ccdc12961e3ca59d11e050
tejaspmarathe <tejaspmarathe1998@gmail.com>
2021-09-03 18:55:08 +0530

    BAC- Bloodbank Search Functionality changes

--- BloodAvailabilityChecker/src/main/java/com/smart/controller/SearchController.java
+++ BloodAvailabilityChecker/src/main/java/com/smart/controller/SearchController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,34 +21,45 @@ public class SearchController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private BLoodStockRepository bLoodStockRepository;
-	
-	
+
+
 	//Search Handler
 	@GetMapping("/search/{query}")
 	public ResponseEntity<?> search(@PathVariable ("query") String query,Principal principal){
-		 
+
 		//User user=this.userRepository.getUserByUserName(principal.getName());
-		
+
 		System.out.println("query ::"+query);
 		String role="ROLE_BLOODBANK";
 		List<User> users = this.userRepository.findDistinctByLocationContainingAndRole(query,role);
-	
-//		List<User> filteredList=filterUniqueRecords(users);
-		
-		return ResponseEntity.ok(users);
+
+		List<User> filteredList=filterUniqueRecords(users);
+
+		return ResponseEntity.ok(filteredList);
 	}
 
 
 	private List<User> filterUniqueRecords(List<User> list) {
 
-		ArrayList<User> users=new ArrayList<User>();
-		list.sort((User s1, User s2)->s1.getLocation().compareTo(s2.getLocation())); 
-		
-		
-		
-		return null;
+		ArrayList<User> users = null;
+		try {
+			list.sort((User s1, User s2)->s1.getLocation().compareTo(s2.getLocation())); 
+
+			Map<String, User> map=new HashMap<String, User>();
+
+			for (User user : list) {
+				if(!map.containsKey(user.getLocation())) {
+					map.put(user.getLocation(), user);
+				}
+			}
+
+			users = new ArrayList<User>(map.values());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return users;
 	}
 }
